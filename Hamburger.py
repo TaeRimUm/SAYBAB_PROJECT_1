1bcaee285104184bd5de6c9adaa222b79280f6b1
TaeRimUm <horse042140@gmail.com>
2022-12-02 05:32:50 +0900

    Update Hamburger.py
    햄버거 먹힐 때 사운드 추가

--- Hamburger.py
+++ Hamburger.py
@@ -6,12 +6,27 @@ import game_world
 
 class hamburger:
     image = None
+    eat_sound = None
+
     def __init__(self):
         print('햄버거 배치')
         if hamburger.image == None:
             hamburger.image = load_image('Hamburger.png')
 
-        self.x, self.y, self.fall_speed = random.randint(400, 1100), random.randint(50, 140), 0
+        if hamburger.eat_sound is None:
+            hamburger.eat_sound = load_wav('nyang.mp3')
+            hamburger.eat_sound.set_volume(32)
+
+        self.x, self.y, self.fall_speed = random.randint(400, 1100), random.randint(50, 600), 0
+
+    def __getstate__(self):
+        state = {'x': self.x, 'y': self.y}
+        return state
+
+    def __setstate__(self, state):
+        self.__init__()
+        self.__dict__.update(state)
+
 
     def get_bb(self):  # 박스의 왼쪽 좌표, 오른쪽 좌표 알려주기(4개의 값을 넘겨주기)
         return self.x - 20, self.y - 30, self.x + 20, self.y + 20
@@ -21,13 +36,11 @@ class hamburger:
         hamburger.image.clip_draw(0, 0, 100, 100, sx, sy)
 
 
-    def do(self):
-        self.x = clamp(0, self.x, 1600)
-
     def update(self):
-        self.y -= self.fall_speed * game_framework.frame_time
+        pass
 
 
     def handle_collision(self, other, group):
         if group == 'hamburger:Skul': #햄버거 입장에서 해골이 부딪히면
+            hamburger.eat_sound.play()
             game_world.remove_object(self)
