3f6e46e637e81b6bf63a259a20ad38d3371fd475
TaeRimUm <horse042140@gmail.com>
2022-12-02 05:31:35 +0900

    Update background.py
    배경음악 추가.

--- background.py
+++ background.py
@@ -12,6 +12,10 @@ class FixedBackground:
         self.w = self.image.w
         self.h = self.image.h
 
+        self.bgm = load_music('ground_music.mp3') #load_music는 mp3하고 ogg를 읽을 수 있음.
+        self.bgm.set_volume(32) # 중요 : ground가 플레이 함. #
+        self.bgm.repeat_play() #반복적으로 플레이
+
     def draw(self):
         self.image.clip_draw_to_origin(self.window_left, self.window_bottom,
                                        self.canvas_width, self.canvas_height,
