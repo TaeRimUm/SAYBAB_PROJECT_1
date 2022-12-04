6efeb22f43537e695e7faa0487b697ff2e72e21f
TaeRimUm <horse042140@gmail.com>
2022-12-04 10:27:49 +0900

    Update saybar.py

--- saybar.py
+++ saybar.py
@@ -8,7 +8,7 @@ start = 0
 
 # Saybar Run Speed
 PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
-RUN_SPEED_KMPH = 20.0  # Km / Hour
+RUN_SPEED_KMPH = 15.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
