5b951282586860eb54e4737df19d409033936877
TaeRimUm <horse042140@gmail.com>
2022-12-04 11:47:14 +0900

    Update game_framework.py

--- game_framework.py
+++ game_framework.py
@@ -100,9 +100,7 @@ def run(start_state):
         stack[-1].update()
         stack[-1].draw()
         frame_time = time.time() - current_time
-        #frame_rate = 1.0 / frame_time
         current_time += frame_time
-        #print("Frame Time : %f sec, Frame Rate: %f fps" % (frame_time, frame_rate))
 
     # repeatedly delete the top of the stack
     while (len(stack) > 0):
